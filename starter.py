f140056fb7d1427c27cde22f4678dad1a99188fd
GenericNerd <genericnerdyt@gmail.com>
2020-01-02 14:16:48 +0000

    Removed files that were not used

--- starter.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-print(f"Started at {datetime.utcnow()}")
-while True:
-    exit_code = os.system("python pxb2.py")
-    if exit_code == 100:
-        print("\nRestarting...")
-    if exit_code == 0:
-        break

5262d33970db9daafed755ff5b46d0a34fecf61b
GenericNerd <genericnerdyt@gmail.com>
2020-01-05 00:03:18 +0000

    Raid Mode fix

--- starter.py
+++ starter.py
@@ -1,6 +1,6 @@
 import os
 while True:
-    exit_code = os.system("py grounddug.py")
+    exit_code = os.system("python grounddug.py")
     if exit_code == 100:
         print("\nRestarting...")
     if exit_code == 0:

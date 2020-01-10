e7abf3b48c6838173eb259db83435cac0c7dfe35
GenericNerd <genericnerdyt@gmail.com>
2020-01-10 17:37:04 +0000

    Async mongodb handle

--- db_handle.py
+++ db_handle.py
@@ -16,26 +16,28 @@ from bson.objectid import ObjectId
 # # # # # # #
 
 client = motor_asyncio.AsyncIOMotorClient("mongodb+srv://Fabio:^P*4k6A$c1I4@grounddug-z0fef.mongodb.net/test?retryWrites=true&w=majority")
+clientnsync = pymongo.MongoClient("mongodb+srv://Fabio:^P*4k6A$c1I4@grounddug-z0fef.mongodb.net/test?retryWrites=true&w=majority")
 db = client.grounddug
+dbnsync = clientnsync.grounddug
 
 # # # # # # #
 # FUNCTIONS #
 # # # # # # #
 
 async def dbUpdate(localdb,finder,update):
-    db[localdb].update_one(finder,{"$set":update})
+    await db[localdb].update_one(finder,{"$set":update})
     
 def dbNSyncFind(localdb,finder):
-    return db[localdb].find_one(finder)
+    return dbnsync[localdb].find_one(finder)
 
 async def dbFind(localdb,finder):
-    return db[localdb].find_one(finder)
+    return await db[localdb].find_one(finder)
 
 async def dbInsert(localdb,data):
-    db[localdb].insert_one(data)
+    await db[localdb].insert_one(data)
 
 async def dbRemove(localdb,finder):
-    db[localdb].delete_one(finder)
+    await db[localdb].delete_one(finder)
 
 async def dbRemoveMany(localdb,finder):
-    db[localdb].delete_many(finder)
+    await db[localdb].delete_many(finder)

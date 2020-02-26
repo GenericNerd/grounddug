a0bafa12b4dce93a397f04b7d873edd4fcfaa47f
GenericNerd <genericnerdyt@gmail.com>
2020-02-26 16:58:01 +0000

    dbFind was never awaited

--- cogs/utils/dbhandle.py
+++ cogs/utils/dbhandle.py
@@ -32,7 +32,7 @@ async def dbRemoveMany(db,finder):
     await asyncdb[db].delete_many(finder)
 
 async def itemExist(db,finder):
-    if dbFind(db,finder) is None:
+    if (await dbFind(db,finder)) is None:
         return True
     else:
         return False

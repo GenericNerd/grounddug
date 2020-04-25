fefc09e5bef44dfc1bc384d234e13826c3b7d6f0
GenericNerd <genericnerdyt@gmail.com>
2020-04-25 21:46:51 +0100

    Fixing on_error

--- grounddug.py
+++ grounddug.py
@@ -52,7 +52,10 @@ async def blacklistChannelCheck(ctx):
 @bot.event
 async def on_error(event,*args,**kwargs):
     # await bot.get_channel(664541295448031295).send(embed=(await embed.generate(f"Error raised! Sentry issue created",None,0xff0000)))
-    capture_exception(event)
+    if isinstance(event,Exception):
+        capture_exception(event)
+    else:
+        logger.error(f"{event}, {*args}, {**kwargs}")
 
 # Check current environment, and run appropriate instance
 if environment == "beta":

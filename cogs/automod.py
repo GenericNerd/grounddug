05ccc3d386ef10711d37580a9e55b54124f3801e
Fabio Almeida <genericnerd.yt@gmail.com>
2020-05-24 16:56:16 +0100

    GDD-3 attemped fix

--- cogs/automod.py
+++ cogs/automod.py
@@ -46,15 +46,20 @@ class AutoModListener(commands.Cog):
                             await cases.createCase(ctx.guild,ctx.author,ctx.guild.me,"message deleted",text.capitalize())
                         # Return an embed with the text variable
                         return await embed.generate(f"{ctx.author.name}#{ctx.author.discriminator} {text} in #{ctx.channel.name}",f"`{ctx.content}`")
+                async def attemptSend(channel, embed):
+                    try:
+                        await channel.send(embed=embed)
+                    except:
+                        pass
                 if not removed:
                     # This is the regex that will be used to check against messages for URLs
                     url_Regex = r"(?:(?:https?|ftp):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))?"
                     # If anti-invite is on and message contains an invite, invoke RuleViolator
                     if guild["automod"]["antiInvite"] and re.search("(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+",ctx.content):
-                        await logChannel.send(embed=(await RuleViolator(ctx,"tried to advertise",True)))
+                        await attemptSend(logChannel,await RuleViolator(ctx,"tried to advertise",True))
                     # If anti-URL is on and the message contains a URL, invoke RuleViolator
                     elif guild["automod"]["antiURL"] and re.search(url_Regex,ctx.content):
-                        await logChannel.send(embed=(await RuleViolator(ctx,"tried to post a link",True)))
+                        await attemptSend(logChannel,await RuleViolator(ctx,"tried to post a link",True))
                     # If short-URL is on and the message contains a URL, check if it is shortened
                     elif guild["automod"]["shortURL"] and re.search(url_Regex,ctx.content):
                         shortened_URLs = []
@@ -70,16 +75,16 @@ class AutoModListener(commands.Cog):
                             description = ""
                             for url in shortened_URLs:
                                 description += f"{url} "
-                            await ctx.channel.send(embed=(await embed.generate("Shortened URLs detected!",f"{ctx.author.mention} posted a shortened link(s) leading to {description}")))
+                            await ctx.channel.send((await embed.generate("Shortened URLs detected!",f"{ctx.author.mention} posted a shortened link(s) leading to {description}")))
                     # If the message contains swearing, invoke RuleViolator
                     elif guild["automod"]["profanity"] and pf.is_profane(ctx.content):
-                        await logChannel.send(embed=(await RuleViolator(ctx,"tried to swear",True)))
+                        await attemptSend(logChannel,await RuleViolator(ctx,"tried to swear",True))
                     # If caps is not disabled, the message is longer than 8 characters and the percentage of caps is above the threshold, invoke RuleViolator
                     elif guild["automod"]["caps"] > 0 and len(ctx.content) > 8 and guild["automod"]["caps"] < (sum(1 for x in ctx.content if str.isupper(x))/len(ctx.content))*100:
-                        await logChannel.send(embed=(await RuleViolator(ctx,"used too many caps",True)))
+                        await attemptSend(logChannel,await RuleViolator(ctx,"used too many caps",True))
                     # If mass mentions are not disabled, and more than mass mentions were mentioned, invoke RuleViolator
                     elif guild["automod"]["massMentions"] > 0 and len(ctx.raw_mentions) >= guild["automod"]["massMentions"]:
-                        await logChannel.send(embed=(await RuleViolator(ctx,"pinged too many people",True)))
+                        await attemptSend(logChannel,await RuleViolator(ctx,"pinged too many people",True))
 
 
 class AutoModSetup(commands.Cog):

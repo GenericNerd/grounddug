88efe01c02be59d264d614d70d88386f369a881e
Fabio Almeida <genericnerd.yt@gmail.com>
2020-05-25 15:27:47 +0100

    GDD-4 initial commit

---
+++ cogs/boundary.py
@@ -0,0 +1,23 @@
+# GroundDug Boundary Cog
+
+import discord
+from discord.ext import commands
+import asyncio
+import uuid
+import cogs.utils.db as db
+import cogs.utils.checks as checks
+
+# Boundary Class
+
+class Boundary(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @bot.command(name="test",description="This is a testing command")
+    @checks.hasRequiredLevel(5)
+    async def test(self,ctx):
+        await db.insert("boundary",{"uuid": uuid.uuid4(),"guild": ctx.guild.id, "user": ctx.author.id})
+        await ctx.send(db.find("boundary",{"guild": ctx.guild.id, "user": ctx.author.id}))
+
+def setup(bot):
+    bot.add_cog(Boundary(bot))

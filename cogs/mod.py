b4d94ee21b08ada1009d8885d09ccc2925476a47
Lewis Crichton <lewistehminerz@outlook.com>
2020-06-13 13:27:22 +0100

    overwrite delete this

--- cogs/mod.py
+++ cogs/mod.py
@@ -134,7 +134,7 @@ class Mod(commands.Cog):
     async def unmute(self,ctx,member:discord.Member,*,reason=None):
         # For each channel, set typing permissions to True for each member
         for channel in ctx.guild.text_channels:
-            await channel.set_permissions(member,send_messages=True)
+            await channel.set_permissions(member,overwrite=None)
         # Send a message in the channel notifying of successful unmute
         await ctx.send(embed=(await embed.generate(f"{member.name} has been unmuted",f"{ctx.author.mention} unmuted this user for: `{reason}`")))
 

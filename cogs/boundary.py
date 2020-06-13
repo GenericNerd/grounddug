2c2a9f82a1b317d5e86c5f39ab148a7f214fafd1
Fabio Almeida <genericnerd.yt@gmail.com>
2020-06-13 13:00:42 +0100

    GDD-4 cleaned up and added comments

--- cogs/boundary.py
+++ cogs/boundary.py
@@ -9,10 +9,15 @@ import cogs.utils.db as db
 import cogs.utils.checks as checks
 import cogs.utils.embed as embed
 
-async def updateBoundary(ctx,state):
+# Create function to enable and disable Boundary db state
+async def updateBoundary(ctx,state,langState):
+    # Find the current guild in the database
+    # Set the parsed state (boolean)
+    # Update the database and return an embed with the contents
     guildDB = await db.find("guilds",{"id": ctx.guild.id})
     guildDB["boundary"]["enabled"] = state
     await db.update("guilds",{"_id": guildDB["_id"]},{"boundary": guildDB["boundary"]})
+    return await embed.generate(f"Boundary is now {langState}!",None,0xffcc4d)
 
 # Boundary Class
 
@@ -22,7 +27,6 @@ class Boundary(commands.Cog):
         self.boundary_check.start()
     
     @commands.group(name="boundary",description="Improve protection on your server")
-    @checks.hasRequiredLevel(5)
     async def boundary(self,ctx):
         if ctx.invoked_subcommand is None:
             await ctx.invoke(self.bot.get_command("help"),"boundary")
@@ -30,21 +34,30 @@ class Boundary(commands.Cog):
     @boundary.command(name="enable",description="| Enable Boundary with current settings")
     @checks.hasGDPermission("ADMINISTRATOR")
     async def enable(self,ctx):
-        await updateBoundary(ctx,True)
+        # Update Boundary to state of True
+        await ctx.send(embed=(await updateBoundary(ctx,True,"enabled")))
 
     @boundary.command(name="disable",description="| Disable Boundary")
     @checks.hasGDPermission("ADMINISTRATOR")
     async def disable(self,ctx):
-        await updateBoundary(ctx,False)
+        # Update Boundary to state of False
+        await ctx.send(embed=(await updateBoundary(ctx,False,"disabled")))
 
-    @boundary.command(name="setrole",description="<role> | Set the role given to users who have verified")
+    @boundary.command(name="setrole",description="[role] | Set the role given to users who have verified")
     @checks.hasGDPermission("ADMINISTRATOR")
-    async def setrole(self,ctx,role:discord.Role):
+    async def setrole(self,ctx,role:discord.Role=None):
+        # If role is not given, return the current role,
+        # otherwise, set the given role to Boundary role
         guildDB = await db.find("guilds",{"id": ctx.guild.id})
-        guildDB["boundary"]["role"] = role.id
-        await db.update("guilds",{"_id": guildDB["_id"]},{"boundary": guildDB["boundary"]})
+        if role == None:
+            await ctx.send(embed=(await embed.generate("Boundary role",f"The current Boundary role is set to: <@{guildDB['boundary']['role']}>",0xffcc4d)))
+        else:  
+            guildDB["boundary"]["role"] = role.id
+            await db.update("guilds",{"_id": guildDB["_id"]},{"boundary": guildDB["boundary"]})
+            await ctx.send(embed=(await embed.generate("Boundary role updated!",f"The new Boundary role has been set to <@{role.id}>",0xffcc4d)))
 
     @boundary.command(name="create",description="This is a testing command")
+    @checks.hasRequiredLevel(5)
     async def test(self,ctx):
         bid = uuid.uuid4()
         await db.insert("boundary",{"uuid": str(bid), "guild": ctx.guild.id, "user": ctx.author.id, "verified": False})
@@ -52,22 +65,26 @@ class Boundary(commands.Cog):
 
     @tasks.loop(seconds=60)
     async def boundary_check(self):
+        # Wait until the bot is ready (mainly to avoid errors on_ready)
         await self.bot.wait_until_ready()
+        # Find and iterate through each verified user
         documents = await db.findAll("boundary",{"verified": True})
         async for document in documents:
+            # Find their member and guild objects (Discord and DB objects)
             guild = self.bot.get_guild(document["guild"])
             user = guild.get_member(document["user"])
             guildDB = await db.find("guilds",{"id": document["guild"]})
-            if guildDB["boundary"]["enabled"] == False:
-                pass
-            elif guildDB["boundary"]["role"] == None and guildDB["channel"] != 0:
-                await guild.get_channel(guildDB["channel"]).send(embed=(await embed.generate("Boundary role not set!",f"You forgot to set a Boundary role! As users verify, the role is not given.\n\n**User**: {user.mention}")))
+            # If the role is not set, but the logs channel is set, send a message alerting mods/admins that Boundary role is not set
+            if guildDB["boundary"]["role"] == None and guildDB["channel"] != 0:
+                await guild.get_channel(guildDB["channel"]).send(embed=(await embed.generate("Boundary role is not set!",f"You forgot to set a Boundary role! As users verify, the role is not given.\n\n**User**: {user.mention}",0xffcc4d)))
             else:
+                # Add the Boundary role to the user and send them a message to let them know they have verified
                 await user.add_roles(guild.get_role(guildDB["boundary"]["role"]))
                 try:
-                    await user.send(embed=(await embed.generate("You have been verified!",f"You are now a verified member in {guild.name}!")))
+                    await user.send(embed=(await embed.generate("You have been verified!",f"You are now a verified member in {guild.name}!",0xffcc4d)))
                 except:
                     pass
+            # Remove the verification document from the database
             await db.remove("boundary",{"_id": document["_id"]})
 
 def setup(bot):

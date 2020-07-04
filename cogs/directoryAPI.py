bfc0b5d4dc2c5da7004d92dfda9c24333f06e1ff
GenericNerdd <genericnerd.yt@gmail.com>
2020-07-04 21:35:04 +0100

    GDD-12 added comments

--- cogs/directoryAPI.py
+++ cogs/directoryAPI.py
@@ -21,6 +21,7 @@ class DirectoryListings(commands.Cog):
     
     def __init(self,bot):
         self.bot = bot
+        # Bot List API keys to interact with the websites
         self.topGGAPI = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU1MzYwMjM1Mzk2MjU0OTI0OSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTkzODg2NTg3fQ.B3Ur3HFLz3M89jzrl0BmTB2yFN9c0RCJq57NovRnY2M"
         self.discordBotsAPI = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOnRydWUsImlkIjoiMTc5MjkyMTYyMDM3NTE0MjQxIiwiaWF0IjoxNTkzODg3MDY2fQ.U-tsOONaxP0RKRh2mf-V0_PyrDFEtGsC7Jqd_FWKZ0Q"
         self.botsForDiscordAPI = "251b7801f049d5dc856ee9d308f2dfadd8870ea398651eb8266b00ba91270b99f06164a60768f80bfaf6a176899e762ef5a2d3f738e7c557406eb2bd5e06c656"
@@ -29,6 +30,7 @@ class DirectoryListings(commands.Cog):
 
     @tasks.loop(minutes=60)
     async def updateStats(self):
+        # Only update APIs if current environment is production
         if environment == "production":
             shards = len(self.bot.latencies)
             guilds = len(self.bot.guilds)

9c690e5b2c37d638aa503497b54e6c36dd53bb96
GenericNerdd <genericnerd.yt@gmail.com>
2020-07-12 15:39:44 +0100

    we try this

--- cogs/utils/db.py
+++ cogs/utils/db.py
@@ -2,23 +2,16 @@
 
 import asyncio
 import pymongo
-from bson.int64 import Int64
 from motor import motor_asyncio
 
-connectionString = "mongodb://root:GQAbun9Tb44kTC1T3XYM8VI3v1DpCwEnQU0GntIdMbMID5V6hI0MAcHKzFZF0f79@195.201.6.72:27017/?authSource=admin&retryWrites=true&w=majority"
+connectionString = "mongodb://grounddug:eXJeX5e4yDPiocwg6mvw5kNDfBB0Bp7k@195.201.6.72:27017/grounddug?authSource=grounddug&retryWrites=true&w=majority"
 # connectionString = "mongodb+srv://GroundDug:qLuVr1KFT8rr29sU@grounddug-z0fef.mongodb.net/test?retryWrites=true&w=majority"
 asyncDBClient = motor_asyncio.AsyncIOMotorClient(connectionString, io_loop=asyncio.get_event_loop())
 nsyncDBClient = pymongo.MongoClient(connectionString)
 asyncDB = asyncDBClient["grounddug"]
 nsyncDB = nsyncDBClient["grounddug"]
-# asyncDB = asyncDBClient
-# nsyncDB = nsyncDBClient
 
 async def find(database,fltr):
-    documents = []
-    async for document in asyncDB[database].find({}):
-        documents.append(document)
-    print(documents)
     return await asyncDB[database].find_one(fltr)
 
 async def findAll(database,fltr):
@@ -53,8 +46,6 @@ async def update(database,fltr,update):
     await asyncDB[database].update_one(fltr,{"$set": update})
 
 def nsyncFind(database,fltr):
-    print(nsyncDB[database].find_one(fltr))
-    print(nsyncDB.guilds.find_one({"id": 526427196072525836}))
     return nsyncDB[database].find_one(fltr)
 
 def nsyncFindAll(database,fltr):

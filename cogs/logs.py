cfe2c32874f00b4c941cc1dd65f8a6be89f9e0b9
GenericNerd <genericnerd.yt@gmail.com>
2020-07-18 01:33:43 +0100

    Make dicts sets

--- cogs/logs.py
+++ cogs/logs.py
@@ -259,8 +259,8 @@ class Logging(commands.Cog):
             if before.type != after.type:
                 msg = await embed.add_field(msg,"Type changed!",f"**Was:** {str(before.type).title()} channel\n**Now:**: {str(after.type).title()}")
             if before.overwrites != after.overwrites:
-                print(before.overwrites-after.overwrites)
-                print(after.overwrites-before.overwrites)
+                print(set(before.overwrites)-set(after.overwrites))
+                print(set(after.overwrites)-set(before.overwrites))
                 for obj, value in list(set(before.overwrites)-set(after.overwrites)):
                     permissionPair = value.pair()
                     permissionsString = ""

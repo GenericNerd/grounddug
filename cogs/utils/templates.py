4f5a5436880b9e052ba5f5c27676580b82e8611c
GenericNerd <genericnerd.yt@gmail.com>
2020-07-24 02:21:39 +0100

    Actually formatting string

--- cogs/utils/templates.py
+++ cogs/utils/templates.py
@@ -12,7 +12,7 @@ environment = jinja2.Environment(
 
 async def purgeTemplate(title,messages):
     template = environment.get_template("purge.html")
-    with open(f"{datetime.utcnow().strftime()}.html","w",encoding="UTF-8") as f:
+    with open(f"{datetime.utcnow().strftime('%d%m%Y%H%M%S')}.html","w",encoding="UTF-8") as f:
         f.write(await template.render_async(title=title,messages=messages))
         f.close()
     return discord.File(f"{datetime.utcnow().strftime()}.html")

bd9ef4e88c346797a8e762b6cc83a9b6711467cb
GenericNerdd <genericnerd.yt@gmail.com>
2020-07-31 13:43:32 +0100

    DB update

--- cogs/developer.py
+++ cogs/developer.py
@@ -74,7 +74,10 @@ class Developer(commands.Cog):
                 del guildObject["logs"]["misc"]
             except:
                 pass
-            await self.bot.get_channel(guildObject["channel"]).send(embed=(await embed.generate("You now have GroundDug Premium!",f"Your GroundDug Premium will expire on {datetime.utcfromtimestamp(guild['premium']['expires']).strftime('%A the %d of %B %Y at %H:%M:%S UTC')}",0x0b9e00)))
+            try:
+                await self.bot.get_channel(guildObject["channel"]).send(embed=(await embed.generate("You now have GroundDug Premium!",f"Your GroundDug Premium will expire on {datetime.utcfromtimestamp(guildObject['premium']['expires']).strftime('%A the %d of %B %Y at %H:%M:%S UTC')}",0x0b9e00)))
+            except:
+                pass
             # for user in guild.members:
             #     userObject = await db.find("users",{"guild": guild.id, "user": user.id})
             #     if userObject is None:
